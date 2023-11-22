ab8b2d0ce1eb4ce2dbe55d0493d4df27318c197a
Emran Binjamaan <therealemran.design@gmail.com>
2023-11-22 13:59:44 +0400

    istg if this doesn't work...

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,50 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ebinjama <ebinjama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 12:09:02 by ebinjama          #+#    #+#             */
-/*   Updated: 2023/11/22 13:53:16 by ebinjama         ###   ########.fr       */
+/*   Updated: 2023/11/22 13:57:54 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
-static int	read_to_buf(int fd, char *buf)
+static int	read_to_buf(int fd, char **buf)
 {
 	int	bytes_read;
 
-	bytes_read = read(fd, buf, BUFFER_SIZE);
+	bytes_read = read(fd, buf[fd], BUFFER_SIZE);
 	buf[bytes_read] = '\0';
 	return (bytes_read);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	buf[BUFFER_SIZE + 1];
+	static char	buf[256][BUFFER_SIZE + 1];
 	char		*line;
 	char		*line_temp;
 	char		*new_line_in_buff;
 
 	line = NULL;
-	new_line_in_buff = ft_strchr(buf, '\n');
+	new_line_in_buff = ft_strchr(buf[fd], '\n');
 	while (!new_line_in_buff)
 	{
-		if (*buf)
+		if (*buf[fd])
 		{
 			line_temp = line;
-			line = ft_strjoin(line, buf);
+			line = ft_strjoin(line, buf[fd]);
 			free(line_temp);
 		}
-		if (!read_to_buf(fd, buf))
+		if (!read_to_buf(fd, buf[fd]))
 			return (line);
-		new_line_in_buff = ft_strchr(buf, '\n');
+		new_line_in_buff = ft_strchr(buf[fd], '\n');
 	}
 	line_temp = line;
-	buf[new_line_in_buff - buf] = '\0';
-	line = ft_strjoin(line, buf);
+	buf[fd][new_line_in_buff - buf[fd]] = '\0';
+	line = ft_strjoin(line, buf[fd]);
 	free(line_temp);
 	ft_strcpy(buf, new_line_in_buff + 1);
 	return (line);

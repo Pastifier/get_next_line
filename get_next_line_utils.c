28eea3c47065d669162c7607f383886d40f2eb4f
Pastifier <therealemran.design@gmail.com>
2023-12-02 23:11:20 +0400

    to test on workspace

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -74,11 +74,8 @@ char	*ft_strdup(const char *str)
 
 char	*ft_strcpy(char *dst, const char *src)
 {
-	size_t	len;
-
 	if (!dst || !src)
 		return (NULL);
-	len = ft_strlen(src);
 	while (*src)
 		*dst++ = *src++;
 	*dst = 0;
--- tests/test.c
+++ tests/test.c
@@ -1,1 +1,16 @@
 #include "../get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void) {
+	int fd = open("test.txt", O_RDONLY);
+	char *line = get_next_line(fd);
+	printf("%s", line);
+	free(line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	//free(line);
+	close(fd);
+}
